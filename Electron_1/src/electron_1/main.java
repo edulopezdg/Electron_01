a97900d517f75da7250a9b1149436f61d9086eb7
Valentin Stieger <stiegervalentin123@gmail.com>
2024-03-20 19:01:08 -0300

    agregue un sout
    updatee jdk y agregue un sout

--- Electron_1/src/electron_1/main.java
+++ Electron_1/src/electron_1/main.java
@@ -16,6 +16,9 @@ public class main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("holanda");
+        
+        /*vale*/
+        System.out.println("hice un upgrade al jdk mio,ahora anda todo");
     }
     
 }
